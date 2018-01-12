2d5cbbf35c4f888153f14bb2bcc026b2c47374a5
Vincent Pitard <vincentpitard.sio@gmail.com>
2018-01-12 17:09:38 +0100

    Ajout Etat

---
+++ src/Etat.java
@@ -0,0 +1,24 @@
+abstract class Etat{
+  protected Serviteur serv;
+
+  public Etat( Serviteur serv )
+  {
+    this.serv = serv;
+  }
+  
+ 
+}
+
+class EtatActif extends Etat{
+
+  public EtatActif( Serviteur serv ){
+    super( serv );
+  }
+}
+
+class EtatSommeil extends Etat{
+
+  public EtatSommeil( Serviteur serv ){
+    super( serv );
+  }
+}
